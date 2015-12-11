35e75115597759737d956174291b8bc908fabccb
Phil Carpenter <Phil Carpenter>
2015-12-10 19:38:18 -0500

    Updat; almost there. GMAIL Authentication subroutines finished

--- org/tempestdesign/sendmail/NoReply.java
+++ org/tempestdesign/sendmail/NoReply.java
@@ -8,26 +8,31 @@ import javax.mail.internet.*;
 import javax.mail.util.*;
 
 public class NoReply {
-
+	
+	
+	// private static String host[] = {"smtp.gmail.com"};
+	private static String host = "smtp.gmail.com";
+	private static String port = "587";
+	private static String mto, msub, mcont;
+	
+	// 
+	//
 	
 	
 	public static void main (String[] args) {
 		//
 		//
 		String user = getUN(),
-				pw = getPW(),
-				mto = getTo(),
-				msub = getSubject(),
-				mtext = getContents(); //
+				pw = getPW();
+				
 		Date mdate = new Date();
 		//
-		String host = "smtp.gmail.com";
-		String port = "587";
+		
 		//
 		Properties prop = new Properties();
 		prop.put("mail.smtp.auth", "true");
 		prop.put("mail.smtp.starttls.enable", "true");
-		prop.put("mail.smtp.host", host);
+		prop.put("mail.smtp.host", host/*[0]*/);
 		prop.put("mail.smtp.port", port);
 		//
 		Session sesh = Session.getInstance(prop,
@@ -39,13 +44,20 @@ public class NoReply {
 		//
 		//
 		try {
-
+			
+			mto = getTo();
+			msub = getSubject();
+			mcont = getContents(); //
+			
 			Message m = new MimeMessage(sesh);
 			m.setFrom(new InternetAddress(user));
-			m.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(mto));
+			m.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mto));
 			m.setSubject(msub);
+			m.setSentDate(mdate);
+			
+			/*
 			m.setText(mtext);
+			*/
 
 			Transport.send(m);
 
@@ -59,20 +71,38 @@ public class NoReply {
 		
 	} // end main //
 	
+	/** NoReply.getUN();
+	 *  
+	 ** >> Prompts user for GMAIL address.
+	 * 
+	 * 
+	 * @return Returns valid GMAIL address as type String.
+	 */
+	
 	private static String getUN() {
 		
+		String UN;
+		Scanner sc = new Scanner(System.in);
+		sc.useDelimiter("(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)"); // Regular Expression to check for valid Gmail username //
 		
-		
-		
-		return null;
+		System.out.println("\t >> Please enter your full gmail address \n"
+					+ "(ex. username@gmail.com, username@google.com, etc. \n)");
+			/*  **************************************  */
+		UN = sc.next();
+		sc.close(); //
+		return UN;
 	} // end getUN() //
 	
 	private static String getPW() {
 		
+		String PW;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("\t >> Please enter your GMAIL account password. \n");
 		
-		
-		return null;
+		PW = sc.next();
+		sc.close(); //
+		return PW;
 	} // end getPW() //
 	
 	private static String getTo() {
@@ -98,18 +128,31 @@ public class NoReply {
 		
 		return null;
 	} // end getContents() //
-	
-	/** *# Scanner subroutines #*
-	 *  *# #*
-	 * 
-	 * 
-	 * 
-	 */
-
-	
-	
+		
 
 }
+
+
+class Contents {
+	
+	public String TEXT, TYPE;
+	
+	private Contents() {
+		
+		
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
 
 
 
