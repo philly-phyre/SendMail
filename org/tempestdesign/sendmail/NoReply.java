2984294dc61399418adf7b0f5e272e38fcbf1f0b
Phil Carpenter <Phil Carpenter>
2015-12-11 12:51:50 -0500

    Update; Major overhaul; Program in great, working condition. Many features still to add, but a very easy-to-use and robust program. Enjoy!

--- org/tempestdesign/sendmail/NoReply.java
+++ org/tempestdesign/sendmail/NoReply.java
@@ -15,6 +15,7 @@ public class NoReply {
 	private static String port = "587";
 	@SuppressWarnings("unused")
 	private static String mto, msub, UN, PW, cTEXT, cTYPE;
+	private static boolean done = false;
 	
 	private static Scanner sc = new Scanner(System.in);
 	// 
@@ -43,31 +44,58 @@ public class NoReply {
 						return new PasswordAuthentication(UN, PW);
 					}
 				}); //
+		chk();
 		//
 		//
-		try {
+		do {
+			try {
+				
+				setTo();
+				setSubject();
+				setContent();
+				
+				Message m = new MimeMessage(sesh);
+				m.setFrom(new InternetAddress(UN));
+				m.addRecipients(Message.RecipientType.TO, InternetAddress.parse(mto)); //////////////////////////////////////////
+				m.setSubject("l");
+				m.setSentDate(mdate);
+				m.setContent(cTEXT, cTYPE);
+				
+	
+				Transport.send(m);
+	
+				
+				
+	
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			} // end try/catch //
 			
-			setTo();
-			setSubject();
-			setContent();
+			sleep(500);
+			chk();
+			System.out.println(">> Your message has been sent successfully!!");
+			sleep(250);
+			System.out.println(">> ?");
+			System.out.println(">> Be sure to check your sent folder, along with your inbox for");
+			System.out.println(">> information regarding unsent messages or errors in sending.");
+			sleep(200);
+			System.out.println(">> ?");
+			System.out.println(">> Would you like to send another message?");
+			System.out.println(">> # [Y/YES/y/yes/1] # [N/NO/n/no/0] #");
 			
-			Message m = new MimeMessage(sesh);
-			m.setFrom(new InternetAddress(UN));
-			m.addRecipients(Message.RecipientType.TO, InternetAddress.parse(mto)); //////////////////////////////////////////
-			m.setSubject("l");
-			m.setSentDate(mdate);
-			m.setContent(cTEXT, cTYPE);
-			
-
-			Transport.send(m);
-
-			System.out.println("Done");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		} // end try/catch //
+			String ck = sc.nextLine().toUpperCase();
+			if (ck.equals("Y") || ck.equals("YES") || ck.equals("1"))
+				;
+			else if(ck.equals("N") || ck.equals("NO") || ck.equals("0"))
+				done = true;
+			else {
+				System.out.println(">> Invalid argument. Exiting [1]");
+				done = true;
+			}
+		} while (!done); // end of program //
 		
-		
+		sc.close();
+		endSesh();
 		
 	} // end main //
 	
@@ -109,7 +137,7 @@ public class NoReply {
 	
 	private static void setPW() {
 		
-		//sleep(900);
+		//sleep(750);
 		System.out.println(">> Please enter your GMAIL account password.");
 				/* **************************************** */
 		PW = sc.nextLine();
@@ -119,18 +147,20 @@ public class NoReply {
 		
 		//sleep(900);
 		System.out.println(">> ?");
-		//sleep(900);
+		//sleep(750);
 		System.out.println(">> Please enter the EMAIL address(es) you would like to send your message to.");
-		//sleep(900);
+		//sleep(750);
 		System.out.println(">> For multiple addresses, separate with a comma and no space.");
-		//sleep(900);
+		//sleep(750);
 		System.out.println(">> (ex. \"me@here.com,you@there.com,them@where.com\")");
+		System.out.println(">> ?");
+		System.out.print(">> ?");
 		try {
 			mto = sc.nextLine();
 		} catch (NoSuchElementException e) {
 			e.printStackTrace();
 		} //
-		
+		chk();
 	} // end getTo() //
 	
 	private static void setSubject() {
@@ -148,16 +178,54 @@ public class NoReply {
 	 */	
 	
 	public static void setContent() {
-		//sleep(900);
-		System.out.println(">> Perfect. Now begin typing your message.");
-		// sleep(900);
+		boolean brk = true;
+		String chk = null;
+		int x = 1;
+		System.out.println(">> This software mostly deals with text-based messages.");
+		//sleep(750);
+		System.out.println(">> Is this a PLAINTEXT message, or an HTMLTEXT message?");
+		//sleep(250);
+		System.out.println(">> \t ## [PLAINTEXT = 1, plain, PLAIN, text, or TEXT] ##");
+		//sleep(250);
+		System.out.println(">> \t ## [HTMLTEXT = 2, html, HTML, code, or CODE");
+		System.out.println(">> ?");
+		System.out.print(">> ?");
+		chk = sc.nextLine().toUpperCase();
+			switch(chk) {
+			case "1":
+			case "PLAIN":
+			case "TEXT":
+				cTYPE = "text/plain";
+				break;
+			case "2":
+			case "HTML":
+			case "CODE":
+				cTYPE="text/html";
+				break;
+			default:
+				System.out.println(">> That was not a valid option; setting to PLAINTEXT by default!!");
+				System.out.println(">> ?");
+				cTYPE = "text/plain";
+			}
+		chk();
+		System.out.println(">> ?");
+		chk = null;
+		//sleep(750);
+		System.out.println(">> Perfect. Now begin composing your message.");
+		// sleep(750);
 		System.out.println(">> You may press RETURN at the end of each sentence or stopping point.");
 		System.out.println(">> RETURN a blank line to end the process and send the email.");
-		while(sc.hasNextLine() && !(sc.nextLine().equals(""))) {
-			cTEXT += sc.toString() + " \n ";
-		}
-		cTYPE = "text/plain";
-		
+		while(brk) {
+			chk = sc.nextLine();
+			cTEXT += chk + " ";
+			if (chk.equals(""))
+				brk = false;
+			else if (x % 3 == 0)
+				cTEXT += " \n";
+			x++;
+			}
+		cTEXT = cTEXT.substring(4);
+		chk();
 	}
 	//
 	//
@@ -169,6 +237,43 @@ public class NoReply {
 			Thread.currentThread().interrupt();
 		}
 	} // end sleep(timeMS) // 
+	
+	public static void chk() {
+		System.out.println(">> ?");
+		System.out.println(">> Check...");
+		System.out.println(">> ?.....");
+		sleep(750);
+		System.out.println(">> Success!");
+		System.out.println(">> ?!!!!!");
+	}
+	
+	public static void endSesh() {
+		System.out.println(">> ?");
+		sleep(111);
+		System.out.println(">> ? #");
+		sleep(111);
+		System.out.println(">> ? ####");
+		sleep(111);
+		System.out.println(">> ? ########");
+		sleep(111);
+		System.out.println(">> ? #############");
+		sleep(111);
+		System.out.println(">> ? ~~# THANK YOU FOR CHOOSING TEMPEST SOFTWARE #~~");
+		sleep(111);
+		System.out.println(">> ? #############");
+		sleep(111);
+		System.out.println(">> ? ########");
+		sleep(111);
+		System.out.println(">> ? ####");
+		sleep(111);
+		System.out.println(">> ? #");
+		sleep(111);
+		System.out.println(">> ?");
+		sleep(333);
+		System.out.println("Exiting [0]");
+		sleep(55);
+		System.exit(0);
+	}
 	
 } // end NoReply class //
 
